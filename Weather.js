6372d51f79fa2d9c610ecfa5cbe9b2d859904ee5
unknown <wldnjs7204@naver.com>
2021-04-01 23:51:15 +0900

    #2.3 Background Gradient 5:00

--- Weather.js
+++ Weather.js
@@ -1,17 +1,46 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, StatusBar } from "react-native";
 import PropTypes from "prop-types";
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { LinearGradient } from 'expo-linear-gradient';
 
+const weatherOptions = {
+    Haze: {
+        iconNmae: "weather-hail"
+    }
+}
 export default function Weather({ temp }) {
     return (
-        <View style={styles.container}>
-            <Text>{temp}</Text>
-        </View>
+        <LinearGradient
+            // Button Linear Gradient
+            colors={['#4c669f', '#3b5998', '#192f6a']}
+            style={styles.container}>
+            <StatusBar barStyle="white-content" />
+            <View style={styles.halfContaioner}>
+                <MaterialCommunityIcons name="weather-rainy" size={90} color="white" />
+                <Text style={styles.temp}>{temp}o</Text>
+            </View>
+            <View style={styles.halfContaioner}>
+            </View>
+        </LinearGradient>
     )
 }
 
 Weather.propTypes = {
-    temp: PropTypes.number.isRequired
+    temp: PropTypes.number.isRequired,
+    condition: PropTypes.oneOf([
+        "Thunderstorm",
+        "Drizzle",
+        "Rain",
+        "Snow",
+        "Atmosphere",
+        "Clear",
+        "Clouds",
+        "Haze",
+        "Mist",
+        "Dust",
+        "Fog"
+    ]).isRequired
 }
 
 const styles = StyleSheet.create({
@@ -19,5 +48,16 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: "center",
         alignItems: "center"
+    },
+    temp: {
+        fontSize: 37,
+        color: "white",
+        fontWeight: "300"
+    },
+    halfContaioner: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+
     }
 })
